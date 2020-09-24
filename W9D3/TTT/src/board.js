c67e5535f2b5d6dc3ca52e9bbdf7075ed81a6502
Omar Hernandez <oah232@gmail.com>
2020-09-23 17:05:18 -0700

    created tictactoe website

--- W9D3/TTT/src/board.js
+++ W9D3/TTT/src/board.js
@@ -31,7 +31,7 @@ class Board {
 
   placeMark(pos, mark) {
     if (!this.isEmptyPos(pos)) {
-      throw new MoveError('Is not an empty position!');
+      raise('Is not an empty position!');
     }
 
     this.grid[pos[0]][pos[1]] = mark;

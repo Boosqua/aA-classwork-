809d60545d764a35e20f46becd3f13529b8c96bc
Omar Hernandez <oah232@gmail.com>
2020-10-08 11:11:07 -0700

    completed initial reducers for bench_bnb

--- W11D3/bench_bnb/bench_bnb/frontend/bench_bnb.jsx
+++ W11D3/bench_bnb/bench_bnb/frontend/bench_bnb.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import configureStore from './store/store';
 /* test */
 import {
   login,
@@ -7,14 +8,17 @@ import {
   signup
 } from "./util/session_api_util"
 
+
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
   ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
-
+  let store = configureStore();
 
 
   /* Test */
-  window.signup = signup
-  window.logout = logout
-  window.login = login
+  window.signup = signup;
+  window.logout = logout;
+  window.login = login;
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
 });
---
+++ W11D3/bench_bnb/bench_bnb/frontend/reducers/session_errors_reducer.js
@@ -0,0 +1,22 @@
+import {
+   RECEIVE_CURRENT_USER,
+   RECEIVE_ERRORS
+} from '../actions/session_actions';
+
+const sessionErrorsReducer = (state = {}, action) => {
+   Object.freeze(state);
+   let newState = Object.assign({}, state);
+
+   switch (action.type) {
+      case RECEIVE_ERRORS:
+         newState.errors = action.errors;
+         return newState;
+      case RECEIVE_CURRENT_USER:
+         newState.errors = null;
+         return newState;
+      default:
+         return state;
+   }
+}
+
+export default sessionErrorsReducer; 

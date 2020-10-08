f8695432ba91c3b08474f5994e7401731d8bd58b
Omar Hernandez <oah232@gmail.com>
2020-10-07 17:08:40 -0700

    added front end auth project

---
+++ W11D3/front_end_auth/frontend/components/session/login.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class Login extends React.Component {
+   constructor(props){
+      super(props)
+   }
+
+   render() {
+      debugger
+      return (
+         <div>
+            <p>Log In!</p>
+         </div>
+      )
+   }
+}
